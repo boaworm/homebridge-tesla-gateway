1a8871e937b6c69b27b44738494c21fdc109ee9d
Henrik Thorburn <boaworm@gmail.com>
2025-01-25 06:23:00 -0600

    Introducing a contact sensor for Grid Contact

--- index.js
+++ index.js
@@ -342,8 +342,14 @@ HTTP_TESLA_GATEWAY.prototype = {
 				}
 
 				// Addition for ContactSensor as Contact with Grid
-				//this.ContactService.getCharacteristic(Characteristic.ContactSensorState).setValue(this.currentGridStatus);
-				this.ContactService.getCharacteristic(Characteristic.ContactSensorState).updateValue(this.currentGridStatus);
+
+
+				// Some randomness for testing
+				const randomStatus = Math.floor( Math.random() * 2);
+				this.log.info("Setting contact sensor to:", randomStatus);
+				this.ContactService.getCharacteristic(Characteristic.ContactSensorState).updateValue(randomStatus);
+
+				// this.ContactService.getCharacteristic(Characteristic.ContactSensorState).updateValue(this.currentGridStatus);
 
 				// End of refresh block
 
